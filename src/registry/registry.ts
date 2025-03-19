8b79475a51bce00f3aca03d51a1dd9657f6ccfa1
Aron Maarek <aron.maarek@edu.devinci.fr>
2025-03-19 11:08:01 +0100

    70%

---
+++ src/registry/registry.ts
@@ -0,0 +1,41 @@
+import bodyParser from "body-parser";
+import express, { Request, Response } from "express";
+import { REGISTRY_PORT } from "../config";
+
+export type Node = { nodeId: number; pubKey: string };
+
+export type RegisterNodeBody = {
+  nodeId: number;
+  pubKey: string;
+};
+
+export type GetNodeRegistryBody = {
+  nodes: Node[];
+};
+
+export async function launchRegistry() {
+  const _registry = express();
+  _registry.use(express.json());
+
+  let nodes: Node[] = [];
+
+  _registry.get("/status", (req, res) => {
+    res.send("live");
+  });
+
+  _registry.post("/registerNode", (req: Request<{}, {}, RegisterNodeBody>, res: Response) => {
+    const { nodeId, pubKey } = req.body;
+    nodes.push({ nodeId, pubKey });
+    res.status(200).send("Node registered");
+  });
+
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.json({ nodes } as GetNodeRegistryBody);
+  });
+
+  const server = _registry.listen(REGISTRY_PORT, () => {
+    console.log(`registry is listening on port ${REGISTRY_PORT}`);
+  });
+
+  return server;
+}
